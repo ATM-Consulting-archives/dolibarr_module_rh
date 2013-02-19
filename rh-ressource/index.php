927b94d9cbd8f65a758c86b5937724709118d9db
privetr <privetr@privetr.(none)>
2013-02-19 17:25:01 +0000

    copie fichier

--- rh-ressource/index.php
+++ rh-ressource/index.php
@@ -1,7 +1,7 @@
 <?php
 
 //TODO ne pas mettre le chemin d'accès en dur.
-require_once('/var/www/dolibarr/htdocs/main.inc.php');
+require_once('/var/www/dolibarr-rh/htdocs/main.inc.php');
 //require_once(DOL_DOCUMENT_ROOT."/main.inc.php");
 //require_once(DOL_DOCUMENT_ROOT."/core/class/html.formfile.class.php");
 //require_once(DOL_DOCUMENT_ROOT."/core/class/html.formother.class.php");
