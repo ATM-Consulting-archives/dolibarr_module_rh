4091b3a914ef1f79e070867cb0e0699acdfdbcbf
delays <delays@delays-OptiPlex-780.(none)>
2013-02-20 17:30:47 +0100

    intégration du calendrier wdCalendar

---
+++ rh-ressource/calendrierRessource.php
@@ -0,0 +1,13 @@
+<?php
+	require('config.php');
+	//require('./class/ressource.class.php');
+	
+	
+	
+	llxHeader('','Calendrier des ressources');
+	
+	require('../../wdCalendar/sample.php'); 
+	
+	
+	llxFooter();
+
