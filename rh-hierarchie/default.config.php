294b75df38a6282d35ded8dcd2f7f85a27ac915e
privetr <privetr@privetr.(none)>
2013-02-26 15:37:53 +0000

    rajout module hierarchie algorithmes ok, css moche... manque liaison avec le menu activation module...

---
+++ rh-hierarchie/default.config.php
@@ -0,0 +1,27 @@
+<?php
+
+	define('ROOT','/var/www/dolibarr-rh/htdocs/');
+	define('COREROOT','/var/www/dolibarr-rh/htdocs/custom/core/');
+	define('COREHTTP','http://127.0.0.1/dolibarr-rh/htdocs/custom/core/');
+	define('HTTP','http://127.0.0.1/dolibarr-rh/');
+
+	if(defined('INC_FROM_CRON_SCRIPT')) {
+		include(ROOT."master.inc.php");
+	}
+	else {
+		include(ROOT."main.inc.php");
+	}
+
+	define('DB_HOST',$dolibarr_main_db_host);
+	define('DB_NAME',$dolibarr_main_db_name);
+	define('DB_USER',$dolibarr_main_db_user);
+	define('DB_PASS',$dolibarr_main_db_pass);
+	define('DB_DRIVER','mysqli');
+
+	define('DOL_PACKAGE', true);
+	define('USE_TBS', true);
+	
+	require(COREROOT.'inc.core.php');
+	
+	define('DOL_ADMIN_USER', 'admin');
+	
---
+++ rh-hierarchie/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//TODO ne pas mettre le chemin d'accès en dur.
+require('config.php');
+//require_once(DOL_DOCUMENT_ROOT."/main.inc.php");
+//require_once(DOL_DOCUMENT_ROOT."/core/class/html.formfile.class.php");
+//require_once(DOL_DOCUMENT_ROOT."/core/class/html.formother.class.php");
+
+llxHeader();
+
+//$form = new Form($db);
+//$formother = new FormOther($db);
+
+?>
+<h1>Bienvenue sur le module Hiérarchie ! </h1>
+
