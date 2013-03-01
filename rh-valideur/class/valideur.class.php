4304547f8debfb4994f9e81397af325a883830e7
Alexis Algoud <alexis@alexis-System-Product-Name.(none)>
2013-03-02 00:05:21 +0100

    mineur

--- rh-valideur/class/valideur.class.php
+++ rh-valideur/class/valideur.class.php
@@ -4,13 +4,23 @@
  */ 
 class TRH_valideur_groupe extends TObjetStd {
 	function __construct() { /* declaration */
+		global $conf;
+		
 		
 		parent::set_table(MAIN_DB_PREFIX.'rh_valideur_groupe');
-		parent::add_champs('type','type=varchar;');				//type de congé
+		parent::add_champs('type','type=varchar;');				//type de valideur
 		parent::add_champs('fk_user,fk_group','type=entier;');	//utilisateur concerné
 		
 		parent::_init_vars();
 		parent::start();
+		
+		
+		$TType =array();
+		
+		// en fonction des droits
+		$TType['NDFP']='Note de frais';
+		
+		$TType['Ressource']='Ressources';
 		
 	}
 }
