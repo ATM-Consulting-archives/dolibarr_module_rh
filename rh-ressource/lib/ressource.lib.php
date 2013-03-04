76221e3b66730b5ff3119bbbc88008d95594e8ee
Alexis Algoud <alexis@atm-consulting.fr>
2013-03-04 11:01:18 +0100

    orientation dev librairie

--- rh-ressource/lib/ressource.lib.php
+++ rh-ressource/lib/ressource.lib.php
@@ -1,9 +1,24 @@
 <?php
 
-function ressourcePrepareHead(&$obj) {
-	return array(
-		array(DOL_URL_ROOT_ALT.'/ressource/typeRessource.php?id='.$obj->getId(), 'Fiche','fiche')
-	);
+function ressourcePrepareHead(&$obj, $type='type-ressource') {
+	
+	switch ($type) {
+		case 'type-ressource':
+				return array(
+					array(DOL_URL_ROOT_ALT.'/ressource/typeRessource.php?id='.$obj->getId(), 'Fiche','fiche')
+				);
+			
+			break;
+		case 'ressource':
+				return array(
+					array(DOL_URL_ROOT_ALT.'/ressource/ressource.php?id='.$obj->getId(), 'Fiche','fiche')
+				);
+			
+			break;
+		
+		
+	}
+	
 	
 }
 	
