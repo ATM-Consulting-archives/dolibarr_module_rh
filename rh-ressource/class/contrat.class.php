a5e70283622f3d10dd7e5665b4faf9a60f309577
delays <delays@delays-OptiPlex-780.(none)>
2013-03-04 10:23:44 +0100

    rien

--- rh-ressource/class/contrat.class.php
+++ rh-ressource/class/contrat.class.php
@@ -7,16 +7,39 @@ class TRH_Contrat  extends TObjetStd {
 		parent::add_champs('date','type=date;');
 		parent::add_champs('duree, kilometrage','type=entier;');
 		
+		parent::add_champs('bail','type=chaine;');
 		parent::add_champs('montant_HT, TVA','type=float;');
 		
 		//Un evenement est lié à une ressource et deux tiers (agence utilisatrice et fournisseur)
-		//TODO fk_tier existe-t-il ?
-		parent::add_champs('fk_tier,entity','type=entier;index;');
-		parent::add_champs('fk_tier,entity','type=entier;index;');
+		parent::add_champs('fk_tier_utilisateur,entity','type=entier;index;');
+		parent::add_champs('fk_tier_fournisseur,entity','type=entier;index;');
 		parent::add_champs('fk_rh_ressource,entity','type=entier;index;');
+		
+		parent::_init_vars();
+		parent::start();
+		
 	}
 	
 	
 }	
 	
-?>
+/*
+ * Classes d'associations
+ * 
+ */
+
+class TRH_Contrat_Ressource  extends TObjetStd {
+	
+	function __construct(){
+		parent::set_table(MAIN_DB_PREFIX.'rh_association_ressourceimport');
+		parent::add_champs('commentaire','type=chaine;');
+		
+		parent::add_champs('fk_rh_contrat,entity','type=entier;index;');
+		parent::add_champs('fk_rh_ressource,entity','type=entier;index;');
+		
+		parent::_init_vars();
+		parent::start();
+	}
+	
+}	
+	
