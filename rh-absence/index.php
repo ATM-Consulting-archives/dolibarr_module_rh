5925c5f98afac1e81aad2bb03fff2509dbf0ffbc
Alexis Algoud <alexis@atm-consulting.fr>
2013-03-04 14:21:49 +0100

    Mise à jour menu congé

--- rh-absence/index.php
+++ rh-absence/index.php
@@ -1,7 +1,10 @@
 <?php
 
-//TODO ne pas mettre le chemin d'accès en dur.
+
 require('config.php');
+require('./lib/absence.lib.php');
+require('./class/absence.class.php');
+
 //require_once(DOL_DOCUMENT_ROOT."/main.inc.php");
 //require_once(DOL_DOCUMENT_ROOT."/core/class/html.formfile.class.php");
 //require_once(DOL_DOCUMENT_ROOT."/core/class/html.formother.class.php");
@@ -11,6 +14,14 @@ llxHeader();
 //$form = new Form($db);
 //$formother = new FormOther($db);
 
-?>
-<h1>Bienvenue sur le module Absences ! </h1>
+$absence = new TRH_Absence;
+print dol_get_fiche_head(absencePrepareHead($absence)  , 'fiche', 'Absence');
 
+?>
+<p align="center">
+	<img src="./img/vacances.jpg" />
+</p>
+<?
+
+
+llxfooter();
