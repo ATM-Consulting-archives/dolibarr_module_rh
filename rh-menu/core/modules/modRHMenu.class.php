5dc307afe379082192c12e8f45fecec903cc5cf2
Alexis Algoud <alexis@atm-consulting.fr>
2013-03-08 18:02:03 +0100

    Correction du theme par defaut

--- rh-menu/core/modules/modRHMenu.class.php
+++ rh-menu/core/modules/modRHMenu.class.php
@@ -208,8 +208,8 @@ class modRHMenu extends DolibarrModules
 		$sql = array();
 
 		dolibarr_set_const($db, 'MAIN_APPLICATION_TITLE', 'SimpleRH', 'chaine', 1, 'Nom imposé par RHMenu', $conf->entity);
-		dolibarr_set_const($db, 'THEME_RH_LOGO', defined('THEME_LOGO') ? THEME_LOGO : './img/logo.png', 'chaine', 1, 'Logo imposé par RHMenu', $conf->entity);
-		dolibarr_set_const($db, 'THEME_RH_BACKGROUND', defined('THEME_BACKGROUND') ? THEME_BACKGROUND : './img/background-galet.jpg', 'chaine', 1, 'fond imposé par RHMenu', $conf->entity);
+		dolibarr_set_const($db, 'THEME_RH_LOGO', defined('THEME_LOGO') ? THEME_LOGO : DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/logo.png', 'chaine', 1, 'Logo imposé par RHMenu', $conf->entity);
+		dolibarr_set_const($db, 'THEME_RH_BACKGROUND', defined('THEME_BACKGROUND') ? THEME_BACKGROUND : DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/background-galet.jpg', 'chaine', 1, 'fond imposé par RHMenu', $conf->entity);
 		dolibarr_set_const($db, 'NDFP_USE_ACCEPT_STATUS', 1, 'entier', 1, 'Mode Status en attende de validation pour NDFP+', $conf->entity);
 
 
