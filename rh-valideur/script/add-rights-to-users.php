b7a2a72211a249d8527b1523cb431c3489483c65
Alexis Algoud <alexis@atm-consulting.fr>
2013-04-08 11:38:14 +0200

    Ajout script de synchro de droit

---
+++ rh-valideur/script/add-rights-to-users.php
@@ -0,0 +1,22 @@
+<?php
+
+	define('INC_FROM_CRON_SCRIPT', true);
+	require('../config.php');
+
+	$ATMdb=new TPDOdb;
+	
+	$TId = TRequeteCore::get_id_from_what_you_want($ATMdb, 'llx_user');
+	
+	foreach($TId as $id) {
+		$user=new User($db);
+		
+		$user->fetch($id);
+		
+		$user->addrights(70301);
+		$user->addrights(70302);
+		$user->addrights(70303);
+		$user->addrights(70305);
+		$user->addrights(70306);
+		
+	}
+	
