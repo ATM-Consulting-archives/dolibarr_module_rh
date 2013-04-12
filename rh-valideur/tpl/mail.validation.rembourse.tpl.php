b25edfe31ada034bc8450bdee26d56100101e39c
Alexis Algoud <alexis@atm-consulting.fr>
2013-04-12 13:51:57 +0200

    Correction envoi de mail

--- rh-valideur/tpl/mail.validation.rembourse.tpl.php
+++ rh-valideur/tpl/mail.validation.rembourse.tpl.php
@@ -2,4 +2,4 @@ Note de Frais
 <hr>
 <p>Bonjour [validation.prenom;strconv=no;protect=no] [validation.nom;strconv=no;protect=no].</p>
 
-<p>Votre note de frais <strong>[validation.ref;strconv=no;protect=no]</strong>, d'un montant de <strong>[validation.total_ttc;strconv=no;protect=no]</strong>, vient d'être <strong>remboursée</strong>.</p>
+<p>Votre note de frais <strong>[validation.ref;strconv=no;protect=no]</strong>, d'un montant de <strong>[validation.total_ttc;strconv=no;protect=no; frm=0,00€]</strong>, vient d'être <strong>remboursée</strong>.</p>
--- rh-valideur/tpl/mail.validation.soumission.tpl.php
+++ rh-valideur/tpl/mail.validation.soumission.tpl.php
@@ -3,4 +3,4 @@ Note de Frais
 
 <p>Bonjour [validation.prenom;strconv=no;protect=no] [validation.nom;strconv=no;protect=no].</p>
 
-<p>Votre note de frais <strong>[validation.ref;strconv=no;protect=no]</strong>, d'un montant de <strong>[validation.total_ttc;strconv=no;protect=no]</strong>, a été correctement prise en compte et vient d'être envoyée au service de validation.</p>
+<p>Votre note de frais <strong>[validation.ref;strconv=no;protect=no]</strong>, d'un montant de <strong>[validation.total_ttc;strconv=no;protect=no; frm=0,00€]</strong>, a été correctement prise en compte et vient d'être envoyée au service de validation.</p>
