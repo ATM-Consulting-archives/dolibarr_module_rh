95e7a299bd9453bbe8e18102aa882774bfd2aae9
Arnaud <pothiera@pothiera@pot.ra>
2013-04-16 16:32:10 +0200

    Petite modification.

--- rh-ressource/documentSupplier.php
+++ rh-ressource/documentSupplier.php
@@ -144,7 +144,6 @@
 		echo $form->end_form();
 		
 		$formfile->form_attach_new_file($_SERVER["PHP_SELF"], '', 0, 0, $can_upload);
-		$formfile->list_of_documents($filearray, $ressource, 'import_fournisseurs', '',0,'import_fournisseurs/',1);
 		
 		?>
 		<script>
