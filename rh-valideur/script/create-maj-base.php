2a5d59a91e261f8e55995b3435e3e5ac39929c81
Arnaud <pothiera@pothiera@pot.ra>
2013-04-16 09:32:56 +0200

    Ajout de l'extrafield "Code analytique" à l'activation du module valideur et modification des exports comptables.

--- rh-valideur/script/create-maj-base.php
+++ rh-valideur/script/create-maj-base.php
@@ -7,6 +7,9 @@
 	
 	require('../config.php');
 	require('../class/valideur.class.php');
+	require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
+	
+	global $db;
 
 	$ATMdb=new Tdb;
 	$ATMdb->db->debug=true;
@@ -16,3 +19,7 @@
 	
 	
 	$ATMdb->Execute("ALTER TABLE `llx_user` ADD code_analytique INT DEFAULT 0");
+	
+	$extrafields = new ExtraFields($db);
+
+	$extrafields->addExtraField ('CODE_ANA', 'Code analytique', 'int', 0, 10, 'user', 0, 0);
