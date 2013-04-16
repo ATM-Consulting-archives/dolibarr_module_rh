13aec162e693dcf0b72e28de90fbeefb3397f4b0
delays <delays@delays-OptiPlex-780.(none)>
2013-04-16 10:32:00 +0200

    fichiers d'imports. Area terminé. Rajout des cartes area dans les types de ressource de base

--- rh-ressource/script/ImportFactureArea.php
+++ rh-ressource/script/ImportFactureArea.php
@@ -14,6 +14,9 @@ global $conf;
 
 $ATMdb=new Tdb;
 
+// relever le point de départ
+$timestart=microtime(true);
+
 $TUser = array();
 $sql="SELECT rowid, name, firstname FROM ".MAIN_DB_PREFIX."user WHERE entity=".$conf->entity;
 $ATMdb->Execute($sql);
@@ -23,7 +26,7 @@ while($ATMdb->Get_line()) {
 
 		
 $idVoiture = getIdTypeVoiture($ATMdb);
-$nomFichier = "./fichierImports/fichier facture total.csv";
+$nomFichier = "./fichierImports/fichier facture area.CSV";
 echo 'Traitement du fichier '.$nomFichier.' : <br><br>';
 
 $TRessource = chargeVoiture($ATMdb);
@@ -34,62 +37,67 @@ $TRessource = chargeVoiture($ATMdb);
 $numLigne = 0;
 if (($handle = fopen($nomFichier, "r")) !== FALSE) {
 	while(($data = fgetcsv($handle, 0,'\r')) != false){
-		echo 'Traitement de la ligne '.$numLigne.'...';
+		//echo 'Traitement de la ligne '.$numLigne.'...';
 		if ($numLigne >=1 ){
 			$infos = explode(';', $data[0]);
-			
-			//print_r($infos);
-			
 			
 			$temp = new TRH_Evenement;
 			$temp->load_liste($ATMdb);
 			$temp->load_liste_type($ATMdb, $temp);
-			if (! array_key_exists ( $infos[9] , $TRessource )){
-				echo 'pas de carte correspondante : '.$infos[9];
+			if (strpos((string) $infos[10], 'Trajet') === FALSE ){
+				echo 'Ligne sans informations à traiter.<br>';
 			}
 			else {
-				$temp->fk_rh_ressource = $TRessource[$infos[9]];
-				if (strpos((string) $infos[17], 'age TVA') !== FALSE ){
-					$temp->type = 'page';
+				if (! array_key_exists ( $infos[6] , $TRessource )){
+					echo 'Pas de carte correspondante : '.$infos[6].'<br>';
 				}
 				else {
-					$temp->type = 'pleindessence';
+					//print_r($infos);
+					$temp->fk_rh_ressource = $TRessource[$infos[6]];
+					$temp->type = 'trajet';
+
+					//$temp->fk_user = $TUser[strtolower($infos[12])];
+					$temp->set_date('date_debut', $infos[11]);
+					$temp->set_date('date_fin', $infos[16]);
+					$temp->coutEntrepriseHT = strtr($infos[22], ',','.');
+					$temp->coutTTC = strtr($infos[24], ',','.');
+					$temp->coutEntrepriseTTC = strtr($infos[24], ',','.');
+					
+					$ttva = array_keys($temp->TTVA , floatval(strtr($infos[21], ',','.')));
+					$temp->TVA = $ttva[0];
+					
+					$temp->numFacture = $infos[4];
+					$temp->compteFacture = $infos[13];
+					
+					$temp->motif = htmlentities('Trajet de '.strtolower($infos[14]).' à '.strtolower($infos[19]), ENT_COMPAT , 'UTF-8');
+					
+					if ($infos[15]=='WE'){
+						$temp->commentaire = 'Utilisation de la carte durant un WE !';	
+					}
+					else {
+						$temp->commentaire = '';	
+					}
+					
+					
+					$temp->save($ATMdb);
+					//echo ' : Ajoutee: sur la carte '.$infos[9];
 				}
-				
-				$temp->fk_user = $TUser[strtolower($infos[12])];
-				
-				$temp->set_date('date_debut', $infos[15]);
-				$temp->set_date('date_fin', $infos[15]);
-				$temp->coutTTC = strtr($infos[19], ',','.');
-				$temp->coutEntrepriseTTC = strtr($infos[19], ',','.');
-				$ttva = array_keys($temp->TTVA,floatval(strtr($infos[25], ',','.')));
-				$temp->TVA = $ttva[0];
-				$temp->coutEntrepriseHT = strtr($infos[20], ',','.');
-				$temp->numFacture = $infos[1];
-				$temp->compteFacture = $infos[13];
-				
-				$temp->motif = htmlentities($infos[17], ENT_COMPAT , 'ISO8859-1');
-				if ($infos[31]!=''){
-					$temp->commentaire = $infos[30].(isset($infos[31]) ? ' kilometrage : '.$infos[31] : '');	
-				}
-				else {
-					$temp->commentaire = $infos[30];	
-				}
-				
-				$temp->save($ATMdb);echo ' : Ajoutee: sur la carte '.$infos[9];
 			}
-			
 		}
 		
-		echo '<br>';
+		//echo ;
 		$numLigne++;
 		
 		//print_r(explode('\n', $data));
 	}
-	
-	echo 'Fin du traitement. '.($numLigne-3).' lignes rajoutés à la table.';
+
+	//Fin du code PHP : Afficher le temps d'éxecution
+	$timeend=microtime(true);
+	$page_load_time = number_format($timeend-$timestart, 3);
+	echo 'Fin du traitement. Durée : '.$page_load_time . " sec";
 	
 }
+
 
 function chargeVoiture(&$ATMdb){
 	global $conf;
@@ -97,10 +105,9 @@ function chargeVoiture(&$ATMdb){
 	$sql="SELECT r.rowid as 'ID', t.rowid as 'IdType', r.numId FROM ".MAIN_DB_PREFIX."rh_ressource as r 
 	LEFT JOIN ".MAIN_DB_PREFIX."rh_ressource_type as t on (r.fk_rh_ressource_type = t.rowid)
 	WHERE r.entity=".$conf->entity."
-	 AND (t.code='voiture' OR t.code='carte') ";
+	 AND (t.code='voiture' OR t.code='cartearea') ";
 	$ATMdb->Execute($sql);
 	while($ATMdb->Get_line()) {
-		//$idVoiture = $ATMdb->Get_field('IdType');
 		$TRessource[$ATMdb->Get_field('numId')] = $ATMdb->Get_field('ID');
 		}
 	return $TRessource;
