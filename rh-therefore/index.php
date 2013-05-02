4a125aa2f52966215224dde9fe40d91d88718fd2
Alexis Algoud <alexis@atm-consulting.fr>
2013-05-02 16:23:57 +0200

    corr cmd

--- rh-therefore/index.php
+++ rh-therefore/index.php
@@ -49,8 +49,8 @@ cp save/$tardoc /mnt/srv1
 		 * 
 		 */
 		 
-		$cmd1 = 'smbclient '.THEREFORE_LOADER.' -W'.THEREFORE_GROUP.' -c "put ./tmp/'.$filename.' ./Loader/'.$filename.'" -U '.THEREFORE_USER.'%'.THEREFORE_PASSWORD;
-		$cmd2 = 'smbclient '.THEREFORE_LOADER.' -W'.THEREFORE_GROUP.' -c "put ./tmp/'.$_FILES['fichier1']['name'].' ./Loader/'.$_FILES['fichier1']['name'].'" -U '.THEREFORE_USER.'%'.THEREFORE_PASSWORD;
+		$cmd1 = 'smbclient '.THEREFORE_LOADER.' -W'.THEREFORE_GROUP.' -c "cd Loader;put ./tmp/'.$filename.'" -U '.THEREFORE_USER.'%'.THEREFORE_PASSWORD;
+		$cmd2 = 'smbclient '.THEREFORE_LOADER.' -W'.THEREFORE_GROUP.' -c "cd Loader;put ./tmp/'.$_FILES['fichier1']['name'].'" -U '.THEREFORE_USER.'%'.THEREFORE_PASSWORD;
 file_put_contents('cmd.log',$cmd1."\n".$cmd2);
 		print $cmd1.'<br/>';
 		print exec($cmd1);
