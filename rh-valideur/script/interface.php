816c9ce8e8c50bf7a52bd83356d0a8171533602b
Arnaud <pothiera@pothiera@pot.ra>
2013-05-02 11:26:17 +0200

    Petite correction sur export compta.

--- rh-valideur/script/interface.php
+++ rh-valideur/script/interface.php
@@ -108,6 +108,35 @@ function _ndf($date_debut, $date_fin){
 				}
                 $m++;
             }
+
+			$sql_ndf = "SELECT";
+    		$sql_ndf.= " CAST(SUM(l.total_ht) as DECIMAL(16,2)) as 'total_ht',";
+			$sql_ndf.= " CAST(SUM(l.total_ttc) as DECIMAL(16,2)) as 'total_ttc',";
+			$sql_ndf.= " n.datee as 'datef'";
+			
+		    $sql_ndf.= " FROM ".MAIN_DB_PREFIX."ndfp_det as l";
+			$sql_ndf.= " LEFT JOIN ".MAIN_DB_PREFIX."ndfp as n ON n.rowid = l.fk_ndfp";
+		    $sql_ndf.= " WHERE n.statut = 1";
+		    $sql_ndf.= " AND n.rowid =".$NDF_ID;
+		    $sql.= " AND (n.dates>='".$date_debut."' AND n.datee<='".$date_fin."')";
+			
+			$resql_ndf=$db->query($sql_ndf);
+			
+			if ($resql_ndf){
+		        $obj_ndf = $db->fetch_object($resql_ndf);
+				
+		        if ($obj_ndf){
+		        	$datef_ndf		=	dol_print_date($obj_ndf->datef,"day");;
+		        	$total_ht_ndf	=	$obj_ndf->total_ht;
+					$total_ttc_ndf	=	$obj_ndf->total_ttc;
+				}
+		    }else{
+		        $error++;
+		        dol_print_error($db);
+		    }
+		    
+			$line = array($ref, "C", $datef_ndf, '', '', '', $total_ht_ndf, $total_ttc_ndf, $code_analytique);
+			$TabNdf[$k]=$line;
         }
     }else{
         $error++;
