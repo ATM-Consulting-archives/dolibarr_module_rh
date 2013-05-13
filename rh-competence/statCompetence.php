92a32543f1e826ed47b1747a3874ef1d8637b7e4
privetr <privetr@privetr.(none)>
2013-05-13 17:05:55 +0200

    correction bugs et debut stats sur les compétences de l'entreprise

--- rh-competence/statCompetence.php
+++ rh-competence/statCompetence.php
@@ -10,6 +10,7 @@
 	$lignecv=new TRH_ligne_cv;
 	$tagCompetence=new TRH_competence_cv;
 
+	
 	if(isset($_REQUEST['action'])) {
 		switch($_REQUEST['action']) {
 			
@@ -21,6 +22,9 @@
 				break;
 		}
 	}
+	else if(isset($_REQUEST['valider'])){
+		_ficheResult($ATMdb,$tagCompetence, 'edit');
+	}
 	else{
 		_fiche($ATMdb,$tagCompetence, 'edit');
 	}
@@ -29,72 +33,7 @@
 	
 	llxFooter();
 	
-	/*
-function _liste(&$ATMdb,  $tagCompetence, $recherche ) {
-	global $langs, $conf, $db, $user;	
-	llxHeader('','Résultat de la recherche');
-	print dol_get_fiche_head(competencePrepareHead($tagCompetence, '')  , '', 'Statistiques');
 	
-	////////////AFFICHAGE DES LIGNES DE CV 
-	$r = new TSSRenderControler($tagCompetence);
-	
-	$sql=$tagCompetence->requeteRecherche($ATMdb, $recherche);
-
-	$TOrder = array('Niveau'=>'DESC');
-	if(isset($_REQUEST['orderDown']))$TOrder = array($_REQUEST['orderDown']=>'DESC');
-	if(isset($_REQUEST['orderUp']))$TOrder = array($_REQUEST['orderUp']=>'ASC');
-				
-	$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 1;			
-	$form=new TFormCore($_SERVER['PHP_SELF'],'formtranslateList','GET');
-	
-	$r->liste($ATMdb, $sql, array(
-		'limit'=>array(
-			'page'=>$page
-			,'nbLine'=>'30'
-		)
-		,'link'=>array(
-			'name'=>'<a href=./experience.php?fk_user=@fkuser@>@val@</a>'
-			,'firstname'=>'<a href="?id=@ID@&action=view">@val@</a>'
-			,'libelleCompetence'=>'<a href="?id=@ID@&action=view">@val@</a>'
-		)
-		,'translate'=>array(
-		)
-		,'hide'=>array('DateCre', 'fk_user', 'rowid', 'fkuser')
-		,'type'=>array('date_debut'=>'date', 'date_fin'=>'date')
-		,'liste'=>array(
-			'titre'=>'RESULTAT DE VOTRE RECHERCHE'
-			,'image'=>img_picto('','title.png', '', 0)
-			,'picto_precedent'=>img_picto('','back.png', '', 0)
-			,'picto_suivant'=>img_picto('','next.png', '', 0)
-			,'noheader'=> (int)isset($_REQUEST['socid'])
-			,'messageNothing'=>"Aucun résultat au sein de l'entreprise pour votre recherche "
-			,'order_down'=>img_picto('','1downarrow.png', '', 0)
-			,'order_up'=>img_picto('','1uparrow.png', '', 0)
-			,'picto_search'=>'<img src="../../theme/rh/img/search.png">'
-			
-		)
-		,'title'=>array(
-			'libelleCompetence'=>'Libellé Compétence'
-			,'name'=>'Utilisateur'
-			//,'firstname'=>'Prénom'
-			
-		)
-		,'search'=>array(
-			'libelleExperience'=>array('recherche'=>'calendar')
-			
-		)
-		,'orderBy'=>$TOrder
-		
-	));
-
-	
-	$form->end();
-	
-	
-	llxFooter();
-}	
-*/
-
 
 function _fiche(&$ATMdb, $tagCompetence,  $mode) {
 	global $db,$user, $langs, $conf;
@@ -174,6 +113,105 @@ function _fiche(&$ATMdb, $tagCompetence,  $mode) {
 		)	
 	);
 	
+	echo $form->end_form();
+	
+	global $mesg, $error;
+	dol_htmloutput_mesg($mesg, '', ($error ? 'error' : 'ok'));
+	llxFooter();
+}
+
+
+function _ficheResult(&$ATMdb, $tagCompetence,  $mode) {
+	global $db,$user, $langs, $conf;
+	llxHeader('','Formations');
+	
+	print dol_get_fiche_head(competencePrepareHead($tagCompetence, '')  , '', 'Statistiques');
+	
+	$form=new TFormCore($_SERVER['PHP_SELF'],'form1','POST');
+	$form->Set_typeaff($mode);
+	echo $form->hidden('fk_user', $user->id);
+	echo $form->hidden('entity', $conf->entity);
+	$fuser = new User($db);
+	$fuser->fetch(isset($_REQUEST['fk_user']) ? $_REQUEST['fk_user'] : $user->id);
+	$fuser->getrights();
+	
+	
+	$idTagRecherche=isset($_REQUEST['libelle']) ? $_REQUEST['libelle'] : 0;
+	$idGroupeRecherche=isset($_REQUEST['groupe']) ? $_REQUEST['groupe'] : 0;
+	$idUserRecherche=isset($_REQUEST['user']) ? $_REQUEST['user'] : 0;
+	
+	if($idGroupeRecherche!=0){	//on recherche le nom du groupe
+		//echo $idGroupeRecherche;exit;
+		$sql="SELECT nom FROM ".MAIN_DB_PREFIX."usergroup
+		WHERE rowid =".$idGroupeRecherche." AND entity=".$conf->entity;
+		$ATMdb->Execute($sql);
+		while($ATMdb->Get_line()) {
+			$nomGroupeRecherche=$ATMdb->Get_field('nom');
+		}
+	}else{
+		$nomGroupeRecherche='Tous';
+	}
+
+	
+	if($idTagRecherche!=0){	//on recherche le nom du tag
+		$sql="SELECT libelleCompetence FROM ".MAIN_DB_PREFIX."rh_competence_cv
+		WHERE rowid =".$idTagRecherche." AND entity=".$conf->entity;
+		$ATMdb->Execute($sql);
+		while($ATMdb->Get_line()) {
+			$nomTagRecherche=$ATMdb->Get_field('libelleCompetence');
+		}
+	}else{
+		$nomTagRecherche='Tous';
+	}
+
+	
+	if($idUserRecherche!=0){	//on recherche le nom de l'utilisateur
+		$sql="SELECT name,  firstname FROM ".MAIN_DB_PREFIX."user
+		WHERE rowid =".$idUserRecherche." AND entity=".$conf->entity;
+		$ATMdb->Execute($sql);
+		while($ATMdb->Get_line()) {
+			$nomUserRecherche=htmlentities($ATMdb->Get_field('firstname'), ENT_COMPAT , 'ISO8859-1')." ".htmlentities($ATMdb->Get_field('name'), ENT_COMPAT , 'ISO8859-1');
+		}
+	}else{
+		$nomUserRecherche='Tous';
+	}
+	
+	//on va obtenir un tableau permettant d'avoir les stats des compétences suivant la recherche
+	$requeteRecherche=$tagCompetence->requeteStatistique($ATMdb, $idGroupeRecherche, $idTagRecherche, $idUserRecherche);
+
+
+	$TBS=new TTemplateTBS();
+	print $TBS->render('./tpl/statCompetenceResult.tpl.php'
+		,array(
+		)
+		,array(
+			'demande'=>array(
+				'idTagRecherche'=>$idTagRecherche
+				,'idGroupeRecherche'=>$idGroupeRecherche
+				,'idUserRecherche'=>$idUserRecherche
+				,'nomTagRecherche'=>$nomTagRecherche
+				,'nomGroupeRecherche'=>$nomGroupeRecherche
+				,'nomUserRecherche'=>$nomUserRecherche
+			)
+			,'resultat'=>array(
+				'total'=>$requeteRecherche['nbUser']
+				,'faible'=>$requeteRecherche['nbUserFaible']*100/$requeteRecherche['nbUser']
+				,'moyen'=>$requeteRecherche['nbUserMoyen']*100/$requeteRecherche['nbUser']
+				,'bon'=>$requeteRecherche['nbUserBon']*100/$requeteRecherche['nbUser']
+				,'excellent'=>$requeteRecherche['nbUserExcellent']*100/$requeteRecherche['nbUser']
+			)
+			,'userCourant'=>array(
+				'id'=>$fuser->id
+				,'nom'=>$fuser->lastname
+				,'prenom'=>$fuser->firstname
+			)
+			,'view'=>array(
+				'mode'=>$mode
+				,'head'=>dol_get_fiche_head(competencePrepareHead($tagCompetence, '')  , '', 'Compétences')
+			)
+		)	
+	);
+
 	echo $form->end_form();
 	
 	global $mesg, $error;
