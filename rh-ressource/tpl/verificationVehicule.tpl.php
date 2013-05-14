0525f08c8311180f6ebfca76cc6255a6a8bae92f
delays <delays@delays-OptiPlex-780.(none)>
2013-05-14 17:31:32 +0200

    Début des vérification des véhicules/contrats

--- rh-ressource/tpl/verificationVehicule.tpl.php
+++ rh-ressource/tpl/verificationVehicule.tpl.php
@@ -1,34 +1,55 @@
 
-<h1>Vérification des véhicules</h1>
+<h1>Vérification des contrats</h1>
 
-Renseignez ici la limite de consommation : [infos.limite;strconv=no;protect=no] L/100km<br><br>
+Contrats dont la fin est comprise entre [infos.date_debut;strconv=no;protect=no] et [infos.date_fin;strconv=no;protect=no] <br><br>
 
 <script>
 
 function ajax(){
 	$.ajax({
-			url: 'script/loadConsommationEssence.php?limite='+$('#limite').val()
+			url: 'script/loadContratLimite.php?plagedebut='+$('#date_debut').val()+'&plagefin='+$('#date_fin').val()
 		}).done(function(data) {
-			liste = JSON.parse(data);
-			$('#content').html('');
+			//liste = JSON.parse(data);
+			//alert(data);
+			/*$('#content').html('');
 			for (var i=0; i<liste.length; i++){
 				$('#content').html($('#content').html()+liste[i].nom+'<br>');
 				$('#content').html($('#content').html()+liste[i].info);
-			}
+			}*/
 		});
 }
-
+/*
 $('#limite').live("keyup", function(){
 	ajax();	
 });
-
+*/
 $(document).ready(function(){
 	ajax();
 });
 
 </script>
 <div id="content">
+	<table class="border" style="text-align:center;width:100%">
+		<tr>
+			<td colspan="5" >Véhicule</td>
+			<td colspan="6" >Contrat associé à ce véhicule</td>
+		</tr>
+		<tr>
+			<td>Société utilisatrice</td>
+			<td>Nom collaborateur</td>
+			<td>Immatriculation</td>
+			<td>Marque</td>
+			<td>Version</td>
+			<td>Montant Loyer</td>
+			<td>Montant assurance</td>
+			<td>Montant entretien</td>
+			<td>Date de début</td>
+			<td>Date de fin</td>
+			<td>Nom Leaseur</td>
+		</tr>
+	</table>
 	
+
 </div>
 
 
--- rh-ressource/verificationVehicule.php
+++ rh-ressource/verificationVehicule.php
@@ -4,25 +4,52 @@
  */ 
 
 require('./config.php');
-//require('./class/evenement.class.php');
-//require('./class/ressource.class.php');
+
+require('./lib/ressource.lib.php');
 global $conf;
 $ATMdb=new TPDOdb;
+
 llxHeader('','Vérification des Véhicules');
 print dol_get_fiche_head(array()  , '', 'Vérification');
 
 
+$idVoiture = getIdType('voiture');
+
+//chargement des voitures
+$TVoitures = getRessource($idVoiture);
+$sql = "SELECT rowid, fk_soc,  immatriculation , marquevoit, modlevoit
+	FROM ".MAIN_DB_PREFIX."rh_ressource` 
+	WHERE entity=".$conf->entity."
+	AND fk_rh_ressource_type =".$idVoiture;
+$ATMdb->Execute($sql);
+while($row = $ATMdb->Get_line()) {
+	$TVoitures[$row->rowid] = array(
+		'societe'=>$row->fk_soc
+		,'fk_user'=>27786//$row->fk_user
+		,'immatriculation'=>$row->immatriculation
+		,'marque'=>$row->marquevoit
+		,'version'=>$row->modlevoit
+		);
+}
+
+print_r($TVoitures);
+
+
+$form=new TFormCore($_SERVER['PHP_SELF'],'form1','POST');
+$form->Set_typeaff('edit');
+	
 $TBS=new TTemplateTBS();
 print $TBS->render('./tpl/verificationVehicule.tpl.php'
 	,array()
 	,array(
 		'infos'=>array(
-			'texte'=>'$texte'
+			'date_debut'=>$form->calendrier('', 'date_debut', '', 12)
+			,'date_fin'=>$form->calendrier('', 'date_fin', '', 12)
 			)
 	)	
 	
 );
 
 
-
+$form->end();
 llxFooter();
