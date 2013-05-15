197233d944f9963c567ac024b1defc6ee7d048b1
delays <delays@delays-OptiPlex-780.(none)>
2013-05-15 17:25:13 +0200

    Vérification consommation essence terminé. Ajax enlevé.

--- rh-ressource/script/loadConsommationEssence.php
+++ rh-ressource/script/loadConsommationEssence.php
@@ -1,4 +1,5 @@
 <?php
+define('INC_FROM_CRON_SCRIPT', true);
 require('../config.php');
 require('../lib/ressource.lib.php');
 global $conf;
@@ -27,25 +28,32 @@ foreach ($TPleins as $idRessource => $value) {
 			$conso = number_format((100*$memLitre)/($km-$memKm),2);
 			if(isset($_REQUEST['limite'])) {
 				if ($conso>=$_REQUEST['limite']){
-					$texte .= "<span style=\"margin-left: 3em;\">".($km-$memKm).'km fait avec '.
-						number_format($memLitre,2).' litres. Conso : '. $conso.'L/100km<br></span>';
+					$texte = ($km-$memKm).'km fait avec '.number_format($memLitre,2).' litres. Conso : '. $conso.'L/100km';
+					//$texte .= "<span style=\"margin-left: 3em;\">".($km-$memKm).'km fait avec '.number_format($memLitre,2).' litres. Conso : '. $conso.'L/100km<br></span>';
 				}
 			}
 			else{
-				$texte .= "<span style=\"margin-left: 3em;\">".($km-$memKm).'km fait avec '.number_format($memLitre,2).' litres. Conso : '. $conso.'L/100km<br></span>';
+				//$texte .= "<span style=\"margin-left: 3em;\">".($km-$memKm).'km fait avec '.number_format($memLitre,2).' litres. Conso : '. $conso.'L/100km<br></span>';
+				$texte = ($km-$memKm).'km fait avec '.number_format($memLitre,2).' litres. Conso : '. $conso.'L/100km';
 			}
 		}
 		$memKm = $km;
 		$memLitre = $litre;
+		if (!empty($texte)){
+			$TRessource[] = array(
+				'nom'=>$TCartes[$idRessource]
+				,'info'=> $texte
+			);
+		}
 	}
-	$TRessource[] = array(
-		'nom'=>$TCartes[$idRessource]
-		,'info'=>empty($texte) ? "<span style=\"margin-left: 3em;\">Aucun dépassement<br></span>" : $texte
-	);
+	
 	
 }
 
-echo json_encode($TRessource);
+
+//echo json_encode($TRessource);
+__out($TRessource);
+
 
 exit();
 
--- rh-ressource/tpl/verificationEssence.tpl.php
+++ rh-ressource/tpl/verificationEssence.tpl.php
@@ -1,10 +1,10 @@
 
 <h1>Vérification des consommations d'essence</h1>
 
-Renseignez ici la limite de consommation : [infos.limite;strconv=no;protect=no] L/100km<br><br>
+Renseignez ici la limite de consommation : [infos.limite;strconv=no;protect=no] L/100km     [infos.valider;strconv=no;protect=no] <br><br>
 
 <script>
-
+/*
 function ajax(){
 	$.ajax({
 			url: 'script/loadConsommationEssence.php?limite='+$('#limite').val()
@@ -25,10 +25,23 @@ $('#limite').live("keyup", function(){
 $(document).ready(function(){
 	ajax();
 });
-
+*/
 </script>
 <div id="content">
-	
+<table class="liste formdoc noborder" style="width:100%">
+	<thead >
+		<tr class="liste_titre">
+			<td>Carte Total</td>
+			<td>Plein d'essence</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>[ressource.nom;block=tr;strconv=no;protect=no]</td>
+			<td>[ressource.info;strconv=no;protect=no]</td>
+		</tr>
+	</tbody>
+</table>
 </div>
 
 
