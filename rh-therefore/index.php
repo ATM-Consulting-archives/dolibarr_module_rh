2c76f9794330697dab8d8b2386ddab9dcd4b7328
Alexis Algoud <alexis@atm-consulting.fr>
2013-05-17 16:08:20 +0200

    test therefore

--- rh-therefore/index.php
+++ rh-therefore/index.php
@@ -69,20 +69,20 @@ Charger un fichier <input type="file" name="fichier1" />
 </iframe> -->
 <?php
 
-//print_r( PDO::getAvailableDrivers());
 	$pdo=new TPDOdb('','odbc:Driver=FreeTDS;Server=192.168.1.117;Database=Therefore; Uid=dolibarr;Pwd=doli2013;');
-	//$pdo = new PDO("odbc:Driver=FreeTDS; Server=sqlsrv4; Port=1433; Database=Therefore; UID=dolibarr; PWD=doli2013;");
-//	$pdo = new PDO("odbc:Driver=FreeTDS;Server=192.168.1.117;Database=Therefore; Uid=dolibarr;Pwd=doli2013;");
-$sql = "SELECT * FROM [Therefore].[dbo].[TheCat".$_REQUEST['categorie']."] WHERE [Id_Dolibarr]=".$_REQUEST['id'];
-// $res=$pdo->prepare("SELECT [DocNp] FROM [Therefore].[dbo].[TheCat13] WHERE [Id_Dolibarr]=".$_REQUEST['id']);
-// $res=$pdo->query("SELECT [*] FROM [Therefore].[dbo].[TheCat".$_REQUEST['categorie']."] WHERE [Id_Dolibarr]=".$_REQUEST['id']);
-//$sql="SELECT [name],[xtype] FROM [Therefore].[dbo].[sysobjects] WHERE xtype='U'";
-// $res=$pdo->prepare("SELECT [name],[xtype] FROM [Therefore].[dbo].[sysobjects] WHERE xtype='U'");
-//$res->execute();
-$pdo->debug=true;
-$pdo->Execute($sql);
 
-$TFichier = $pdo->Get_All();
-pre($TFichier);
-//print_r( $pdo->errorInfo());
+	$sql = "SELECT * FROM [Therefore].[dbo].[TheCat".$_REQUEST['categorie']."]";// WHERE [Id_Dolibarr]=".$_REQUEST['id'];
+	$pdo->debug=true;
+	$pdo->Execute($sql);
+
+	$TFichier = $pdo->Get_All();
+	//pre($TFichier);
+
+	$liste=new TListviewTBS('fichiers');
+	$liste->renderArray($pdo, $TFichier,array(
+	
+	
+	));
+	
+
 	llxFooter();
