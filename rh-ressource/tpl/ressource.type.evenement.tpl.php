c4fc6f497564316b339ba30d2f0402596329e7f5
delays <delays@delays-OptiPlex-780.(none)>
2013-05-21 09:39:45 +0200

    Création des types d'événements. Ecrans et fonctionnalités terminés.

--- rh-ressource/tpl/ressource.type.evenement.tpl.php
+++ rh-ressource/tpl/ressource.type.evenement.tpl.php
@@ -8,8 +8,8 @@
 	
 			
 <table width="100%" class="border">
-	<tr><td width="20%">Code</td><td>[ressourceType.code; strconv=no]</td></tr>
 	<tr><td width="20%">Libellé</td><td>[ressourceType.libelle; strconv=no]</td></tr>
+	<tr><td width="20%">Code</td><td>[ressourceType.code; strconv=no]</td></tr>
 </table>
 <br>
 
@@ -24,6 +24,33 @@
 		<td>Code</td>
 		<td>[newEvent.code;strconv=no]</td>
 	</tr>
+	
+	<script>
+		String.prototype.sansAccent = function(){
+		    var accent = [
+		        /[\300-\306]/g, /[\340-\346]/g, // A, a
+		        /[\310-\313]/g, /[\350-\353]/g, // E, e
+		        /[\314-\317]/g, /[\354-\357]/g, // I, i
+		        /[\322-\330]/g, /[\362-\370]/g, // O, o
+		        /[\331-\334]/g, /[\371-\374]/g, // U, u
+		        /[\321]/g, /[\361]/g, // N, n
+		        /[\307]/g, /[\347]/g, // C, c
+		    ];
+		    var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c',''];
+		    var str = this;
+		    for(var i = 0; i < accent.length; i++){
+		        str = str.replace(accent[i], noaccent[i]);
+		        str = str.replace(' ','');
+		    }
+    		return str;
+		}
+		
+		$('#libelle').change(function(){
+			texte = $('#libelle').val();
+			$('#code').val(texte.sansAccent());			
+		})
+		
+	</script>
 	<tr>
 		<td>Code Analytique</td>
 		<td>[newEvent.codeanalytique;strconv=no]</td>
@@ -34,8 +61,10 @@
 
 [onshow;block=begin;when [view.mode]!='edit']		
 	<div class="tabsAction">
-		<a href="?id=[ressourceType.id]&idRegle=[newRule.id]&action=edit" class="butAction">Modifier</a>
-		<a class="butActionDelete"  href="?id=[ressourceType.id]&idRegle=[newRule.id]&action=delete">Supprimer</a>
+		<a href="?id=[ressourceType.id]&idTypeEvent=[newEvent.id]&action=edit" class="butAction">Modifier</a>
+		[onshow;block=begin;when [newEvent.supprimable]=='vrai']		
+			<a class="butActionDelete"  href="?id=[ressourceType.id]&idTypeEvent=[newEvent.id]&action=delete">Supprimer</a>
+		[onshow;block=end]	
 	</div>
 [onshow;block=end]	
 [onshow;block=begin;when [view.mode]=='edit']
