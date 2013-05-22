d653c4013d1f8299c25fa77c529dfa3df7654042
delays <delays@delays-OptiPlex-780.(none)>
2013-05-22 14:49:53 +0200

    liste User ave In (o, conf entity)

--- rh-ressource/class/evenement.class.php
+++ rh-ressource/class/evenement.class.php
@@ -73,7 +73,7 @@ class TRH_Evenement  extends TObjetStd {
 		
 		//chargement d'une liste de touts les users (pour le combo "Utilisateur")
 		$this->TUser = array();
-		$sqlReq="SELECT rowid, firstname, name FROM ".MAIN_DB_PREFIX."user WHERE entity=".$conf->entity;
+		$sqlReq="SELECT rowid, firstname, name FROM ".MAIN_DB_PREFIX."user WHERE entity IN (0,".$conf->entity.")";
 		$ATMdb->Execute($sqlReq);
 		while($ATMdb->Get_line()) {
 			$this->TUser[$ATMdb->Get_field('rowid')] = htmlentities($ATMdb->Get_field('firstname'), ENT_COMPAT , 'ISO8859-1')." ".htmlentities($ATMdb->Get_field('name'), ENT_COMPAT , 'ISO8859-1'); 
