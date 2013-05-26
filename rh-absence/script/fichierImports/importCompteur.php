f9117de8de0c62bae27fabed70e432040e037000
PI05 <pi05@pi05-Latitude-D830.(none)>
2013-05-26 09:01:01 +0200

    modifiation import compteur

--- rh-absence/script/fichierImports/importCompteur.php
+++ rh-absence/script/fichierImports/importCompteur.php
@@ -3,8 +3,8 @@
 set_time_limit(0);
 ini_set("memory_limit", "512M");
 
-require('../config.php');
-require('../class/absence.class.php');
+require('../../config.php');
+require('../../class/absence.class.php');
 
 
 global $conf;
@@ -39,7 +39,7 @@ while($ATMdb->Get_line()) {
 
 
 //----------------DEBUT DU TRAITEMENT DES LIGNES D'APPELS----------------------------------------------------------
-$nomFichier = "./fichierImports/compteurSalaries.csv";
+$nomFichier = "./compteurSalaries.csv";
 echo 'Traitement du fichier '.$nomFichier.' : <br><br>';
 
 //début du parsing
@@ -71,16 +71,16 @@ if (($handle = fopen($nomFichier, "r")) !== FALSE) {
 				
 				
 				//RTT Cumulés
-				$resteRttCumule=$infos[18]-$infos[21];
+				$prisRttCumule=$infos[18]-$infos[21];
 				echo "RTT Cumulés acquis : ".$infos[18].' <br/>';	//colonne S
-				echo "RTT Cumulés pris : ".$resteRttCumule.' <br/>';	//colonne S-V
+				echo "RTT Cumulés pris : ".$prisRttCumule.' <br/>';	//colonne S-V
 				echo "RTT Cumulés A poser : ".$infos[21].' <br/>';	//colonne V
 			
 				
 				//RTT Non cumulés
-				$resteRttNonCumule=$infos[17]-$infos[20];
+				$prisRttNonCumule=$infos[17]-$infos[20];
 				echo "RTT Non Cumulés acquis : ".$infos[17].' <br/>';	//colonne R
-				echo "RTT Non Cumulés pris : ".$resteRttNonCumule.' <br/>';	//colonne R-U
+				echo "RTT Non Cumulés pris : ".$prisRttNonCumule.' <br/>';	//colonne R-U
 				echo "RTT Non Cumulés A poser : ".$infos[20].' <br/>';	//colonne U
 				
 				
@@ -91,38 +91,51 @@ if (($handle = fopen($nomFichier, "r")) !== FALSE) {
 				//traitement des lignes et insertion en base
 				
 				//on récupère le compteur de l'utilisateur si celui-ci existe sinon il sera créé
-				/*$compteur->load_by_fkuser($ATMdb, $infos[3]);
-				
+				$compteur->load_by_fkuser($ATMdb,$TUser[strtolower($infos[3])]);
+
 				$annee=date('Y');
 				$anneePrec=$annee-1;
 		
-				$compteur->acquisExerciceN=$infos[13]; 
+				
 				//$compteur->acquisAncienneteN=0;
 				//$compteur->acquisHorsPeriodeN=0;
 				$compteur->anneeN=$annee;
+				$compteur->anneeNM1=$anneePrec;
+				
+				
+				//RTT cumulés
+				$compteur->rttCumulePris=$prisRttCumule;
+				$compteur->rttAcquisAnnuelCumuleInit=$infos[18];
+				
+				
+				//RTT non cumulés
+				$compteur->rttNonCumulePris=$prisRttNonCumule;
+				$compteur->rttAcquisAnnuelNonCumuleInit=7;
+				
+				//congés
 				$compteur->acquisExerciceNM1=$infos[11];
+				$compteur->congesPrisNM1=$infos[12];
+				$compteur->acquisExerciceN=$infos[13]; 
+				
+				
 				//$compteur->acquisAncienneteNM1=0;
 				//$compteur->acquisHorsPeriodeNM1=0;
 				//$compteur->reportCongesNM1=0;
-				$compteur->congesPrisNM1=$infos[12];
-				$compteur->anneeNM1=$anneePrec;
-				$compteur->rttPris=0;
-				$compteur->rttTypeAcquisition='Annuel';
-				$compteur->rttAcquisMensuelInit=0;
-				$compteur->rttAcquisMensuelTotal=0;
-				$compteur->rttAcquisAnnuelCumuleInit=5;
-				$compteur->rttAcquisAnnuelNonCumuleInit=7;
-				$compteur->rttAcquisMensuel=0;
-				$compteur->rttAcquisAnnuelCumule=5;
-				$compteur->rttAcquisAnnuelNonCumule=7;
-				$compteur->rttMetier='cadre';
+				
+				//$compteur->rttTypeAcquisition='Annuel';
+				//$compteur->rttAcquisMensuelInit=0;
+				
+				//$compteur->rttAcquisMensuel=0;
+				//$compteur->rttAcquisAnnuelCumule=5;
+				//$compteur->rttAcquisAnnuelNonCumule=7;
+				//$compteur->rttMetier='cadre';
 				$compteur->rttannee=$annee;
 				$compteur->nombreCongesAcquisMensuel=2.08;
 				$compteur->date_rttCloture=strtotime('2013-03-01 00:00:00'); // AA Ne devrait pas être en dur mais en config
 				$compteur->date_congesCloture=strtotime('2013-06-01 00:00:00');
-				$compteur->reportRtt=0;
-				$compteur->entity = $conf->entity;
-				
+				//$compteur->reportRtt=0;
+
+				exit;
 				//$compteur->save($ATMdb);*/
 				
 			}	
