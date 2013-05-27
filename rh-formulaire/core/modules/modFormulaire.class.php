7653421aee0b6bdaf5e73236ac6a8d30ec02be13
Alexis Algoud <alexis@atm-consulting.fr>
2013-05-27 16:03:51 +0200

    Mineur sur formulaire, test avec user non superAdmin OK

--- rh-formulaire/core/modules/modFormulaire.class.php
+++ rh-formulaire/core/modules/modFormulaire.class.php
@@ -91,7 +91,7 @@ class modFormulaire extends DolibarrModules
 		$this->dirs = array();
 
 		// Config pages. Put here list of php page, stored into mymodule/admin directory, to use to setup module.
-		$this->config_page_url = array("admin.php@formulaire");
+		$this->config_page_url = array("../limesurvey/admin@formulaire");
 
 		// Dependencies
 		$this->depends = array();		// List of modules id that must be enabled if this module is enabled
