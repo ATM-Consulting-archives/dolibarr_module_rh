dc05b36158c607bf83c93b4233da3e5ab7801978
Arnaud <pothiera@pothiera@pot.ra>
2013-05-29 14:13:48 +0200

    Petites modifs.

--- rh-valideur/script/create-maj-base.php
+++ rh-valideur/script/create-maj-base.php
@@ -25,9 +25,7 @@
 	//$ATMdb->Execute("ALTER TABLE `llx_user` ADD code_analytique INT DEFAULT 0");
 	
 	$extrafields = new ExtraFields($db);
-
 	$extrafields->addExtraField('COMPTE_TIERS', 'Compte tiers', 'varchar', 0, 10, 'user', 0, 0);
 	$extrafields->addExtraField('DDN', 'Date de naissance', 'date', 0, 10, 'user', 0, 0);
 	$extrafields->addExtraField('SIT_FAM', 'Situation de famille', 'varchar', 0, 255, 'user', 0, 0);
 	$extrafields->addExtraField('NB_ENF_CHARGE', 'Nombre d\'enfants à charge', 'int', 0, 10, 'user', 0, 0);
-	$extrafields->addExtraField('DDA', 'Date d\'ancienneté', 'date', 0, 10, 'user', 0, 0);
