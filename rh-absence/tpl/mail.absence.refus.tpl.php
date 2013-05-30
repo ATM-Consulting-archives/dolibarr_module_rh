f76a33b180e9a6b6b3c2ffd0d48520dd8d977bdc
privetr <privetr@privetr.(none)>
2013-05-30 14:55:30 +0200

    import compteurs fini mails finis

--- rh-absence/tpl/mail.absence.refus.tpl.php
+++ rh-absence/tpl/mail.absence.refus.tpl.php
@@ -2,4 +2,5 @@
 
 <p>Votre demande de <strong>[absence.libelle;strconv=no;protect=no]</strong> du [absence.date_debut;strconv=no;protect=no] au [absence.date_fin;strconv=no;protect=no] vient d'être [absence.libelleEtat;strconv=no;protect=no]</p>
 
+<p> Commentaire du supérieur lié au refus :  [absence.commentaireValideur;strconv=no;protect=no]</p>
 
