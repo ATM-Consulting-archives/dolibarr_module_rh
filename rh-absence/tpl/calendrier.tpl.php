e359f17e052efaca3400939d0ca053baaaab0dee
privetr <privetr@privetr.(none)>
2013-06-04 11:09:51 +0200

    amélioration interface

--- rh-absence/tpl/calendrier.tpl.php
+++ rh-absence/tpl/calendrier.tpl.php
@@ -3,13 +3,25 @@
 		[view.titreCalendar;strconv=no;protect=no] 	
 		
 		[onshow;block=begin;when [absence.droits]=='1']
+			<table class="liste border" style="width:100%">			
+				<tr>
+					<td >Groupe</td>
+					<td>[absence.TGroupe;strconv=no;protect=no]</td>
+				<tr/>
+				<tr>
+					<td>Utilisateur</td>
+					<td>[absence.TUser;strconv=no;protect=no]</td>
+				<tr/>
+				<tr>
+					<td>Type</td>
+					<td> [absence.TTypeAbsence;strconv=no;protect=no]</td>
+				<tr/>
+				<tr>
+					<td></td>
+				 	<td colspan="2">[absence.btValider;strconv=no;protect=no] </td>
+				<tr/>
+			</table>
 		 	<br> 
-					Groupe  [absence.TGroupe;strconv=no;protect=no]
-		            Utilisateur    [absence.TUser;strconv=no;protect=no]
-		            Type    [absence.TTypeAbsence;strconv=no;protect=no]
-	
-		                [absence.btValider;strconv=no;protect=no] 
-		    <br>
 		[onshow;block=end]
 	
 		<script>
@@ -27,9 +39,6 @@
 				});
 		});
 		</script>
-		<br/><br/>
-			
-
 
 		
 			<div id="agenda">
