15b12fb5465eb67d98fa64eb078384b50aba20a8
Arnaud <pothiera@pothiera@pot.ra>
2013-06-06 11:17:32 +0200

    Modifications pour BSI.

--- rh-valideur/script/interface.php
+++ rh-valideur/script/interface.php
@@ -202,14 +202,13 @@ function _situation_perso(&$ATMdb, $userId){
 	$TabRecapSituationPerso=array();
 	
 	$sql="SELECT e.DDN as 'ddn', e.SIT_FAM as 'situation_famille', e.NB_ENF_CHARGE as 'nb_enfants'
-	FROM ".MAIN_DB_PREFIX."user_extrafields as e 
-		LEFT JOIN ".MAIN_DB_PREFIX."user as u ON (e.fk_object = u.rowid)
-	WHERE u.entity=".$conf->entity."
-	AND u.rowid=".$userId;
+	FROM ".MAIN_DB_PREFIX."user_extrafields as e
+	WHERE e.fk_object=".$userId;
 	
 	$ATMdb->Execute($sql);
 	while($ATMdb->Get_line()) {
-		$TabRecapSituationPerso['ddn']=dol_print_date($ATMdb->Get_field('ddn'));
+		$ddn=$ATMdb->Get_field('ddn');
+		$TabRecapSituationPerso['ddn']=substr($ddn,8,2)."/".substr($ddn,5,2)."/".substr($ddn,0,4);
 		$TabRecapSituationPerso['situation_famille']=$ATMdb->Get_field('situation_famille');
 		$TabRecapSituationPerso['nb_enfants']=$ATMdb->Get_field('nb_enfants');
 	}
@@ -225,12 +224,43 @@ function _situation_pro(&$ATMdb, $userId){
 	
 	$sql="SELECT u.job as 'fonction'
 	FROM ".MAIN_DB_PREFIX."user as u
-	WHERE u.entity=".$conf->entity."
-	AND u.rowid=".$userId;
+	WHERE u.rowid=".$userId;
 	
 	$ATMdb->Execute($sql);
 	while($ATMdb->Get_line()) {
 		$TabRecapSituationPro['fonction']=$ATMdb->Get_field('fonction');
+	}
+	
+	$sql="SELECT r.date_entreeEntreprise
+	FROM ".MAIN_DB_PREFIX."rh_remuneration as r
+	WHERE r.fk_user=".$userId;
+	
+	$ATMdb->Execute($sql);
+	while($ATMdb->Get_line()) {
+		$date_anciennete=$ATMdb->Get_field('date_entreeEntreprise');
+		$TabRecapSituationPro['date_anciennete']=substr($date_anciennete,8,2)."/".substr($date_anciennete,5,2)."/".substr($date_anciennete,0,4);
+	}
+	
+	$sql="SELECT e.HORAIRE as 'horaire', e.STATUT as 'statut', e.NIVEAU as 'niveau', e.CONTRAT as 'contrat'
+	FROM ".MAIN_DB_PREFIX."user_extrafields as e
+	WHERE e.fk_object=".$userId;
+	
+	$ATMdb->Execute($sql);
+	while($ATMdb->Get_line()) {
+		$TabRecapSituationPro['horaire']=$ATMdb->Get_field('horaire');
+		$TabRecapSituationPro['statut']=$ATMdb->Get_field('statut');
+		$TabRecapSituationPro['niveau']=$ATMdb->Get_field('niveau');
+		$TabRecapSituationPro['contrat']=$ATMdb->Get_field('contrat');
+	}
+	
+	$sql="SELECT e.label
+	FROM ".MAIN_DB_PREFIX."entity as e
+	LEFT JOIN ".MAIN_DB_PREFIX."user as u ON (u.entity=e.rowid)
+	WHERE u.rowid=".$userId;
+	
+	$ATMdb->Execute($sql);
+	while($ATMdb->Get_line()) {
+		$TabRecapSituationPro['affectation']=$ATMdb->Get_field('label');
 	}
 	
 	return $TabRecapSituationPro;
