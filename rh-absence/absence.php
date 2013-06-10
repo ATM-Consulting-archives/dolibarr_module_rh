64c15e8d007cd1445bbc36f49af70ddf6dd87f86
privetr <privetr@privetr.(none)>
2013-06-10 14:16:58 +0200

    changement droit par défaut changement ordre des listes users

--- rh-absence/absence.php
+++ rh-absence/absence.php
@@ -591,11 +591,17 @@ function _fiche(&$ATMdb, &$absence, $mode) {
 
 	//création du tableau des utilisateurs liés au groupe du valideur, pour créer une absence, pointage...
 	$TUser = array();
+	$sql="SELECT rowid, name,  firstname FROM `".MAIN_DB_PREFIX."user` WHERE rowid=".$user->id;
+	$ATMdb->Execute($sql);
+	if($ATMdb->Get_line()){
+		$TUser[$ATMdb->Get_field('rowid')]=ucwords(strtolower(htmlentities($ATMdb->Get_field('name'), ENT_COMPAT , 'ISO8859-1')))." ".htmlentities($ATMdb->Get_field('firstname'), ENT_COMPAT , 'ISO8859-1');
+	}
+
 	if($user->rights->absence->myactions->creerAbsenceCollaborateur){
-		$sqlReqUser="SELECT rowid, name,  firstname FROM `".MAIN_DB_PREFIX."user`";
+		$sql="SELECT rowid, name,  firstname FROM `".MAIN_DB_PREFIX."user`";
 		$droitsCreation=1;
 	}else if($user->rights->absence->myactions->creerAbsenceCollaborateurGroupe){
-		$sqlReqUser=" SELECT DISTINCT u.fk_user,s.rowid, s.name,  s.firstname 
+		$sql=" SELECT DISTINCT u.fk_user,s.rowid, s.name,  s.firstname 
 			FROM `".MAIN_DB_PREFIX."rh_valideur_groupe` as v, ".MAIN_DB_PREFIX."usergroup_user as u, ".MAIN_DB_PREFIX."user as s  
 			WHERE v.fk_user=".$user->id." 
 			AND v.type='Conges'
@@ -606,8 +612,8 @@ function _fiche(&$ATMdb, &$absence, $mode) {
 		$droitsCreation=1;
 	}else $droitsCreation=2; //on n'a pas les droits de création
 	if($droitsCreation==1){
-		$sqlReqUser.=" ORDER BY name";
-		$ATMdb->Execute($sqlReqUser);
+		$sql.=" ORDER BY name";
+		$ATMdb->Execute($sql);
 		while($ATMdb->Get_line()) {
 			$TUser[$ATMdb->Get_field('rowid')]=ucwords(strtolower(htmlentities($ATMdb->Get_field('name'), ENT_COMPAT , 'ISO8859-1')))." ".htmlentities($ATMdb->Get_field('firstname'), ENT_COMPAT , 'ISO8859-1');
 		}
