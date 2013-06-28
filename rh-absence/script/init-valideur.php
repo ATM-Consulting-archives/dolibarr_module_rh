f4f766a005ac27578f3ca54eb13257ba8a475bbe
Alexis Algoud <alexis@atm-consulting.fr>
2013-06-28 17:10:32 +0200

    corr

--- rh-absence/script/init-valideur.php
+++ rh-absence/script/init-valideur.php
@@ -10,7 +10,33 @@ llx_user u LEFT OUTER JOIN llx_rh_valideur_groupe uvg ON (uvg.`fk_user`=u.rowid 
 WHERE u.rowid IN (SELECT fk_user FROM llx_user) AND uvg.rowid IS NULL");
 
 	$TUser = $ATMdb->Get_All();
-	print_r($TUser);
 	foreach($TUser as $row) {
 		
+		$fk_user = $row['rowid'];
+		
+		$fuser=new User($db);
+		$fuser->fetch($fk_user); 
+		
+		$groupname =  'Groupe de validation congés (hiérarchique) de '.$fuser->login;
+		
+		$group = new UserGroup($db);
+		if($group->fetch('',$groupname)) {
+			$idGroup=$group->id;
+		}
+		
+		print "Ajout du droit validatio congé de  ".$fuser->login." dans ".$groupname."<br>";
+		
+		$v=new TRH_valideur_groupe;
+		$v->type = 'Conges';
+		$v->fk_user = $fk_user;
+		$v->entity = $conf->entity;
+		$v->level = 1;
+		
+		$v->fk_usergroup = $idGroup;
+		
+		/*	parent::add_champs('type','type=chaine;');				//type de valideur
+		parent::add_champs('nbjours','type=entier;');			//nbjours avant alerte
+		parent::add_champs('montant','type=float;');			//montant avant alerte
+		parent::add_champs('fk_user,fk_usergroup,entity,validate_himself,pointeur,level','type=entier;index;');	//utilisateur ou groupe concerné
+		*/
 	}
