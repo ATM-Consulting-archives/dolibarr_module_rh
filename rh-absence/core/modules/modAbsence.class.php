5f6981172b7f9dbe1f6048376cb201feaaac73cf
Alexis Algoud <alexis@atm-consulting.fr>
2013-07-03 17:07:56 +0200

    ajout droit voir son EDT

--- rh-absence/core/modules/modAbsence.class.php
+++ rh-absence/core/modules/modAbsence.class.php
@@ -207,6 +207,13 @@ class modAbsence extends DolibarrModules
         $this->rights[$r][5] = 'voirTousEdt';
 		$r++;
 		
+		$this->rights[$r][0] = 7118;
+		$this->rights[$r][1] = 'Visualiser son emploi du temps';
+		$this->rights[$r][3] = 0;
+		$this->rights[$r][4] = 'myactions';
+        $this->rights[$r][5] = 'voirSonEdt';
+		$r++;
+		
 		$this->rights[$r][0] = 7107;
 		$this->rights[$r][1] = 'Modifier l\'emploi du temps des collaborateurs';
 		$this->rights[$r][3] = 0;
@@ -361,7 +368,7 @@ class modAbsence extends DolibarrModules
 					'langs'=> 'absence@absence',	// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 					'position'=> 102,
 					'enabled'=> '1',			// Define condition to show or hide menu entry. Use '$conf->mymodule->enabled' if entry must be visible if module is enabled.
-					'perms'=> '$user->rights->absence->myactions->voirTousEdt',			// Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
+					'perms'=> '$user->rights->absence->myactions->voirTousEdt || $user->rights->absence->myactions->voirSonEdt',			// Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
 					'target'=> '',
 					'user'=> 2
         );
