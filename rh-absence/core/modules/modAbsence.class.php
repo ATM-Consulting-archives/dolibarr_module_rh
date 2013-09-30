d4f200574fcd7209e5c6972d13edb33e98ed065a
Alexis Algoud <alexis@atm-consulting.fr>
2013-09-30 15:58:50 +0200

    Remise en place du menu règle absence

--- rh-absence/core/modules/modAbsence.class.php
+++ rh-absence/core/modules/modAbsence.class.php
@@ -547,20 +547,24 @@ class modAbsence extends DolibarrModules
         );
 		
 		$r++;
+		
 		$this->menu[$r]=array(
 		            'fk_menu'=>'fk_mainmenu=absence',			// Put 0 if this is a top menu
 		        	'type'=> 'left',			// This is a Top menu entry
-		        	'titre'=> $langs->trans('Documents règles absences'),
-		        	'mainmenu'=> '',
-		        	'leftmenu'=> '',		// Use 1 if you also want to add left menu entries using this descriptor. Use 0 if left menu entries are defined in a file pre.inc.php (old school).
+		        	'titre'=> 'Documents règles absence',
+		        	'mainmenu'=> 'absence',
+		        	'leftmenu'=> 'test',		// Use 1 if you also want to add left menu entries using this descriptor. Use 0 if left menu entries are defined in a file pre.inc.php (old school).
 					'url'=> '/absence/documentRegle.php',
 					'langs'=> 'absence@absence',	// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-					'position'=> 105,
+					'position'=> 115,
 					'enabled'=> '1',			// Define condition to show or hide menu entry. Use '$conf->mymodule->enabled' if entry must be visible if module is enabled.
 					'perms'=> '',			// Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
 					'target'=> '',
 					'user'=> 2
         );
+		
+		$r++;
+		
 
 		 $this->menu[$r]=array(
 		            'fk_menu'=>'fk_mainmenu=report,fk_leftmenu=exports',			// Put 0 if this is a top menu
