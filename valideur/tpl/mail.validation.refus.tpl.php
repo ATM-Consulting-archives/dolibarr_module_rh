0add33f6ae881c3b6990f4af3b5735ecbd3ba232
Alexis Algoud <alexis@atm-consulting.fr>
2013-11-21 12:05:49 +0100

    Ajout message de refus de validation

--- valideur/tpl/mail.validation.refus.tpl.php
+++ valideur/tpl/mail.validation.refus.tpl.php
@@ -3,3 +3,4 @@ Note de Frais
 <p>Bonjour [validation.prenom;strconv=no;protect=no] [validation.nom;strconv=no;protect=no]</p>
 
 <p>Votre note de frais <strong>[validation.ref;strconv=no;protect=no]</strong>, d'un montant de <strong>[validation.total_ttc;strconv=no;protect=no; frm=0,00€]</strong>, vient d'être <strong>refusée</strong>.</p>
+<p><strong>Motif : </strong><br />[validation.motif;magnet=p]</p>
