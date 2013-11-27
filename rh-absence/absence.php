9a3cb0c3be657e7a7d2027391f60be2215ce051c
Alexis Algoud <alexis@atm-consulting.fr>
2013-11-27 09:25:11 +0100

    Erreur sur la liste d'absence à Validée

--- rh-absence/absence.php
+++ rh-absence/absence.php
@@ -455,7 +455,7 @@ function _listeValidation(&$ATMdb, &$absence) {
 		//LISTE DES ABSENCES À VALIDER
 		$r = new TSSRenderControler($absence);
 		
-		$TOrder = array('DateCre'=>'DESC');
+		$TOrder = array('date_cre'=>'DESC');
 		if(isset($_REQUEST['orderDown']))$TOrder = array($_REQUEST['orderDown']=>'DESC');
 		if(isset($_REQUEST['orderUp']))$TOrder = array($_REQUEST['orderUp']=>'ASC');
 					
