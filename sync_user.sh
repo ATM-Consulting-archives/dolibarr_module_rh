2c37785253f5a7be85360a20c43112a74a51245d
root <root@SRVDOLIBARR.groupecpro.local>
2013-12-18 09:54:48 +0100

    sync_user

---
+++ sync_user.sh
@@ -0,0 +1,12 @@
+cd /var/www/dolibarr-rh/scripts/user/
+./sync_users_ldap2dolibarr.php commitiferror > /dev/null
+
+
+./sync_groups_ldap2dolibarr.php commitiferror > /dev/null
+
+
+./sync_valideur_ldap2dolibarr.php commitiferror  > /dev/null
+
+
+
+
