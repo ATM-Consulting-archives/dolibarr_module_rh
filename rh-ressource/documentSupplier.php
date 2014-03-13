7aa1ce1d8ac1993d2729de8e232ea079345d5910
Alexis Algoud <alexis@atm-consulting.fr>
2014-03-13 11:12:23 +0100

    ajout entité

--- rh-ressource/documentSupplier.php
+++ rh-ressource/documentSupplier.php
@@ -182,10 +182,12 @@
 		<?php
 		$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 1;
 		
-		$sql = "SELECT idImport as idImport, count(*) as 'Nombre de lignes', rowid as ID, date_cre, '' as Action FROM ".MAIN_DB_PREFIX."rh_evenement
-				WHERE idImport IS NOT NULL AND idImport != ''
-				GROUP BY idImport
-				ORDER BY date_cre DESC";
+		$sql = "SELECT e.idImport as idImport, count(*) as 'Nombre de lignes', e.rowid as ID, e.date_cre, ent.label as 'entity', '' as Action 
+		
+			FROM ".MAIN_DB_PREFIX."rh_evenement e LEFT JOIN ".MAIN_DB_PREFIX."entity ent ON (e.entity=ent.rowid)
+				WHERE e.idImport IS NOT NULL AND e.idImport != ''
+				GROUP BY e.idImport
+				ORDER BY e.date_cre DESC";
 		
 		$l=new TListviewTBS('listImports');
 		
@@ -200,6 +202,7 @@
 			,'title'=>array(
 				'idImport' => 'Intitulé du fichier importé'
 				,'date_cre'=> 'Importé le'
+				,'entity'=>'Entité'
 			)
 			,'type'=>array(
 				'date_cre'=>'date'
