4f780f50ff7cba3ff8051b0822602acfcd3a5612
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-03-21 10:47:28 +0100

    Correction congé le jour même + decompte après vérification

--- rh-absence/absence.php
+++ rh-absence/absence.php
@@ -26,19 +26,22 @@
 				$existeDeja=$absence->testExisteDeja($ATMdb, $absence);
 				if($existeDeja===false){
 					$absence->code=saveCodeTypeAbsence($ATMdb, $absence->type);
-					$demandeRecevable=$absence->testDemande($ATMdb, $_REQUEST['fk_user'], $absence);
-				
-					if (($absence->date_debut < time() ||$absence->date_fin<time() )
-					 && (!$user->rights->absence->myactions->creerAbsenceCollaborateur && !TRH_valideur_groupe::isValideur($ATMdb, $user->id))){
-//print (int)TRH_valideur_groupe::isValideur($ATMdb, $user->id);
+					
+					// Test de la cohérence des dates
+					if(!$user->rights->absence->myactions->creerAbsenceCollaborateur 
+					
+					&& ($absence->date_debut <= strtotime('midnight') ||$absence->date_fin <= strtotime('midnight') )) {
 						/*
 							Si ce n'est pas un user avec droit, pas le droit de créer des anciennes absences						
 						*/
 						$mesg = '<div class="error">Attention : seul un utilisateur avec pouvoir peut créer une absence antérieure à maintenant.</div>';
 						_fiche($ATMdb, $absence,'edit');
+						break;
 					} 
 					
-					else if($demandeRecevable==1){
+					$demandeRecevable=$absence->testDemande($ATMdb, $_REQUEST['fk_user'], $absence);
+				
+					if($demandeRecevable==1){
 						$absence->save($ATMdb);
 						$absence->load($ATMdb, $_REQUEST['id']);
 						if($absence->fk_user==$user->id){	//on vérifie si l'absence a été créée par l'user avant d'envoyer un mail
