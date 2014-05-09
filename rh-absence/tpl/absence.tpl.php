f10f8002fdf64dc8a65d62b087439a3dc9477ca1
Alexis Algoud <alexis@atm-consulting.fr>
2014-05-09 11:50:14 +0200

    oublis colonne

--- rh-absence/tpl/absence.tpl.php
+++ rh-absence/tpl/absence.tpl.php
@@ -145,6 +145,8 @@
 					<td><b>Date de début</b></td>
 					<td><b>Date de fin</b></td>
 					<td><b>Type d'absence</b></td>
+					<td><b>Durée décomptée</b></td>
+					<td><b>Congés disponible avant</b></td>
 					<td><b>Etat</b></td>
 				</tr>
 				<tbody id="TRecapAbs">
