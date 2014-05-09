1454bf5be377a0e3a0cc448a497a213072627f1e
root <root@SRVDOLIBARR.groupecpro.local>
2014-05-09 17:48:58 +0200

    Ticket#769

--- rh-ressource/script/ImportFactureEuromaster.php
+++ rh-ressource/script/ImportFactureEuromaster.php
@@ -248,8 +248,8 @@ function chargeVoiture(&$ATMdb){
 	$TRessource = array();
 	$sql="SELECT r.rowid as 'ID', t.rowid as 'IdType', r.numId FROM ".MAIN_DB_PREFIX."rh_ressource as r 
 	LEFT JOIN ".MAIN_DB_PREFIX."rh_ressource_type as t on (r.fk_rh_ressource_type = t.rowid)
-	WHERE r.entity=".$conf->entity."
-	 AND (t.code='voiture' OR t.code='cartearea') ";
+	WHERE 1 AND (t.code='voiture' OR t.code='cartearea') "; // r.entity=".$conf->entity."
+
 	$ATMdb->Execute($sql);
 	while($ATMdb->Get_line()) {
 		$TRessource[$ATMdb->Get_field('numId')] = $ATMdb->Get_field('ID');
