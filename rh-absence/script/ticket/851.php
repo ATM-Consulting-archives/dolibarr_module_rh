5b185a4d4530de523599ca6fd2e742f9caed09be
root <root@SRVDOLIBARR.groupecpro.local>
2014-06-11 10:25:53 +0200

    Debug 851

--- rh-absence/script/ticket/851.php
+++ rh-absence/script/ticket/851.php
@@ -2,7 +2,9 @@
 
 	require('../../config.php');
 	
-	$f1=fopen('851.cvs','r')  or die('fichier');
+ini_set('display_errors',1);
+
+	$f1=fopen('851.csv','r') or die('fichier');
 	
 	$ATMdb=new TPDOdb;
 	
@@ -13,13 +15,13 @@
 		
 		$cp = $row[4];
 		
-		$cpreport_reel = $row[5];
+		$cpreport_reel = abs(strtr($row[5],',','.'));
 		
 		
 		$fk_user = _get_fk_user($ATMdb, $nom, $prenom);
 		
 		$sql=" UPDATE ".MAIN_DB_PREFIX."rh_compteur 
-				SET congesPrisNM1 = congesPrisNM1 - (reportCongesNM1 - $cpreport_reel), reportCongesNM1=$cpreport_reel
+				SET congesPrisNM1 = congesPrisNM1 - (reportCongesNM1 + $cpreport_reel), reportCongesNM1=".(-$cpreport_reel)."
 				WHERE fk_user=$fk_user;
 		";
 	
@@ -29,12 +31,22 @@
 	
 function _get_fk_user(&$ATMdb, $nom, $prenom) {
 
-	$Tab  = $ATMdb->ExecuteAsArray("SELECT rowid FROM ".MAIN_DB_PREFIX."user WHERE name=".$ATMdb->quote($nom)." AND fistname=".$ATMdb->quote($prenom)."");
+$trans=array(' '=>'%');
+$prenom = strtr($prenom, $trans);
+$nom = strtr($nom, $trans);
+
+$sql = "SELECT rowid
+FROM ".MAIN_DB_PREFIX."user
+WHERE name LIKE ".$ATMdb->quote($nom)." AND firstname LIKE ".$ATMdb->quote($prenom)." AND statut=1";
+//print $sql;
+	$Tab  = $ATMdb->ExecuteAsArray($sql);
 
 	if(count($Tab)!=1) {
-		var_dump($Tab, $nom, $prenom);
+//		var_dump($Tab, $nom, $prenom);
 		
-		exit('Erreur');
+// exit('Erreur'); 
+
+return -1;
 	}
 	else {
 		return $Tab[0]->rowid;
