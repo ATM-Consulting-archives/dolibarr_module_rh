dfadba4af09b674eb9ea697b5c19f292cf6e9538
Alexis Algoud <alexis@atm-consulting.fr>
2014-07-08 16:37:39 +0200

    corr 956

--- rh-absence/script/ticket/956.php
+++ rh-absence/script/ticket/956.php
@@ -16,8 +16,8 @@
 	while($obj = $ATMdb->Get_line()) {
 		
 		$nb_conges = _get_conges($ATMdb, $obj->fk_user);
-		$nb_rtt = _get_conges($ATMdb, $obj->fk_user,"'rttcumule'");
-		$nb_rttnon = _get_conges($ATMdb, $obj->fk_user,"'rttnoncumule'");
+		$nb_rtt = _get_conges($ATMdb, $obj->fk_user,"'rttcumule'",'2014-01-01');
+		$nb_rttnon = _get_conges($ATMdb, $obj->fk_user,"'rttnoncumule'",'2014-01-01');
 
 		
 		$sql=" UPDATE ".MAIN_DB_PREFIX."rh_compteur 
@@ -29,9 +29,9 @@
 		
 	}
 
-function _get_conges(&$ATMdb, $fk_user, $type="'conges','cppartiel'") {
+function _get_conges(&$ATMdb, $fk_user, $type="'conges','cppartiel'", $date= '2014-06-01') {
 
-	$sql="SELECT SUM(duree) as nb FROM llx_rh_absence WHERE fk_user=$fk_user AND type IN ($type) AND date_debut>='2014-06-01' AND etat!='Refusee' ";
+	$sql="SELECT SUM(duree) as nb FROM llx_rh_absence WHERE fk_user=$fk_user AND type IN ($type) AND date_debut>='$date' AND etat!='Refusee' ";
 
 	$ATMdb->Execute($sql);
 	$ATMdb->Get_line();
