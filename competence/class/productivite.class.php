71dd0758bb40de7ba8f015cc7532bb29a26c4919
Gauthier <gauthier.verdol@atm-consulting.fr>
2014-09-30 12:02:05 +0200

    maj tables gestion productivité

--- competence/class/productivite.class.php
+++ competence/class/productivite.class.php
@@ -5,22 +5,39 @@ class TRH_productivite extends TObjetStd {
 		
 		parent::set_table(MAIN_DB_PREFIX.'rh_productivite');
 		parent::add_champs('date_objectif','type=date;');
-		parent::add_champs('objectifIndice1,objectifIndice2,objectifIndice3','type=float;');
+		parent::add_champs('indice','type=float;');
 		
-		parent::add_champs('objectif');
+		parent::add_champs('label');
 		
-		
-		parent::add_champs('fk_user','type=entier;');
 		parent::add_champs('entity','type=entier;');
 		
 		parent::_init_vars();
 		parent::start();
 	}
 
+}
 
-	function load_by_user(&$ATMdb, $fk_user){
-		$sql="SELECT rowid FROM ".MAIN_DB_PREFIX."rh_productivite 
-		WHERE fk_user=".$fk_user;
+
+class TRH_productiviteUser extends TObjetStd {
+	function __construct() { 
+		
+		parent::set_table(MAIN_DB_PREFIX.'rh_productivite_user');
+		parent::add_champs('date_objectif','type=date;');
+		parent::add_champs('indice','type=float;');
+		
+		parent::add_champs('objectif');
+		
+		
+		parent::add_champs('fk_user,fk_productivite','type=entier;index;');
+		
+		parent::_init_vars();
+		parent::start();
+	}
+
+
+	function load_by_user(&$ATMdb, $fk_user, $fk_productivite){
+		$sql="SELECT rowid FROM ".$this->get_table()." 
+		WHERE fk_user=".$fk_user." AND fk_productivite=".$fk_productivite;
 		$ATMdb->Execute($sql);
 		if ($ATMdb->Get_line()) {
 			return $this->load($ATMdb, $ATMdb->Get_field('rowid'));
@@ -37,7 +54,7 @@ class TRH_productiviteIndice extends TObjetStd {
 		
 		parent::set_table(MAIN_DB_PREFIX.'rh_productivite_indice');
 		parent::add_champs('date_indice','type=date;');
-		parent::add_champs('indice1,indeice2,indice3','type=float;');
+		parent::add_champs('indice','type=float;');
 			
 		parent::add_champs('fk_user,fk_productivite,entity','type=entier;');
 		
@@ -45,16 +62,5 @@ class TRH_productiviteIndice extends TObjetStd {
 		parent::start();
 	}
 
-
-	function load_by_user(&$ATMdb, $fk_user){
-		$sql="SELECT rowid FROM ".MAIN_DB_PREFIX."rh_productivite 
-		WHERE fk_user=".$fk_user;
-		$ATMdb->Execute($sql);
-		if ($ATMdb->Get_line()) {
-			return $this->load($ATMdb, $ATMdb->Get_field('rowid'));
-		}
-		return false;
-		
-	}
 
 }
