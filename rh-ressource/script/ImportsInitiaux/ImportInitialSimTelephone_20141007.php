8c69e1350cb0194427da8aea1d33a18e44d19b4c
Gauthier <gauthier.verdol@atm-consulting.fr>
2014-10-08 16:11:27 +0200

    maj script et new file

--- rh-ressource/script/ImportsInitiaux/ImportInitialSimTelephone_20141007.php
+++ rh-ressource/script/ImportsInitiaux/ImportInitialSimTelephone_20141007.php
@@ -4,7 +4,7 @@
 	dol_include_once('/ressource/class/ressource.class.php');
 	dol_include_once('/ressource/class/evenement.class.php');
 	
-	$nomFichier = "affectation_sim_telephones.csv";
+	$nomFichier = "affectation_sim_telephones_2.csv";
 	
 	$ATMdb = new TPDOdb;
 	
@@ -12,24 +12,34 @@
 		
 		$osef = fgetcsv($handle, "", ";");
 		$osef = fgetcsv($handle, "", ";");
+		$osef = fgetcsv($handle, "", ";");
 		
 		while(($data = fgetcsv($handle, "", ";")) != false){
 			
 			// Si le code compta est différent de "N/A" ou "?"
-			if($data[3] !== "N/A" && $data[3] !== "?") {
-				
-				$id_user = _get_user($data);
-				if($id_user !== false) {
-					
-					$id_tel = _add_tel($data, $ATMdb);
-					_add_carte_sim($data, $ATMdb, $id_tel);
-					_add_affectation_user_tel($ATMdb, $id_tel, $id_user);
-					
-				}
-				
-			}
+			$TDonnees[] = $data;
 			
 		}
+		
+	}
+	
+	$data = _get_formatted_array($TDonnees);
+	var_dump($data);exit;
+	function _get_formatted_array($data) {
+		
+		$TResult = array();
+		
+		foreach($data as $line) {
+			
+			if(!empty($line[5])) $TResult["33".$line[3]]['num_sim'] = $line[5];
+			if(!empty($line[6])) $TResult["33".$line[3]]['imei'] = $line[6];
+			if(!empty($line[7])) $TResult["33".$line[3]]['puk'] = $line[7];
+			if(!empty($line[9])) $TResult["33".$line[3]]['TUser'] = explode(" ", $line[9]);
+			if(!empty($line[20])) $TResult["33".$line[3]]['lib_phone'] = $line[20];
+			
+		}
+		
+		return $TResult;
 		
 	}
 	
@@ -86,3 +96,81 @@
 		$emprunt->save($ATMdb);
 		
 	}
+
+
+	// Ajout à la main de ceux qui n'ont pas d'email
+	
+	//ALONSO FRANCOIS
+	// Tél
+	$sim = new TRH_Ressource;
+	$sim->fk_rh_ressource_type = 4; // Tél
+	$sim->libelle = "Téléphone 33".$data[1];
+	$sim->numerotel = "33".$data[1];
+	$sim->save($ATMdb);
+
+	// SIM
+	$sim = new TRH_Ressource;
+	$sim->fk_rh_ressource_type = 5; // SIM
+	$sim->libelle = "Carte SIM 33".$data[1];
+	$sim->numerotel = "33".$data[1];
+	$sim->fk_rh_ressource = "33".$id_tel;
+	$sim->save($ATMdb);
+
+	// Emprunt
+	$emprunt = new TRH_Evenement;
+	$emprunt->type = "emprunt";
+	$emprunt->fk_rh_ressource = $id_tel;
+	$emprunt->fk_user = 28511;
+	$emprunt->save($ATMdb);
+	
+	
+	
+	
+	//CLEMENT BRUN
+	// Tél
+	$sim = new TRH_Ressource;
+	$sim->fk_rh_ressource_type = 4; // Tél
+	$sim->libelle = "Téléphone 33".$data[1];
+	$sim->numerotel = "33".$data[1];
+	$sim->save($ATMdb);
+
+	// SIM
+	$sim = new TRH_Ressource;
+	$sim->fk_rh_ressource_type = 5; // SIM
+	$sim->libelle = "Carte SIM 33".$data[1];
+	$sim->numerotel = "33".$data[1];
+	$sim->fk_rh_ressource = "33".$id_tel;
+	$sim->save($ATMdb);
+
+	// Emprunt
+	$emprunt = new TRH_Evenement;
+	$emprunt->type = "emprunt";
+	$emprunt->fk_rh_ressource = $id_tel;
+	$emprunt->fk_user = $id_user;
+	$emprunt->save($ATMdb);
+	
+	
+	
+	
+	//BESSON QUENTIN
+	// Tél
+	$sim = new TRH_Ressource;
+	$sim->fk_rh_ressource_type = 4; // Tél
+	$sim->libelle = "Téléphone 33".$data[1];
+	$sim->numerotel = "33".$data[1];
+	$sim->save($ATMdb);
+
+	// SIM
+	$sim = new TRH_Ressource;
+	$sim->fk_rh_ressource_type = 5; // SIM
+	$sim->libelle = "Carte SIM 33".$data[1];
+	$sim->numerotel = "33".$data[1];
+	$sim->fk_rh_ressource = "33".$id_tel;
+	$sim->save($ATMdb);
+
+	// Emprunt
+	$emprunt = new TRH_Evenement;
+	$emprunt->type = "emprunt";
+	$emprunt->fk_rh_ressource = $id_tel;
+	$emprunt->fk_user = $id_user;
+	$emprunt->save($ATMdb);
