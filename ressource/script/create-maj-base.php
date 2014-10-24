83248180db69ae4f8fe1e14b68e69c04fd7e597f
Alexis Algoud <alexis@atm-consulting.fr>
2014-10-24 10:37:58 +0200

    Portage import orange depuis 3.3

--- ressource/script/create-maj-base.php
+++ ressource/script/create-maj-base.php
@@ -10,6 +10,7 @@
 	require('../class/contrat.class.php');
 	require('../class/evenement.class.php');
 	require('../class/regle.class.php');
+	require('../class/numeros_speciaux.class.php');
 
 	$ATMdb=new TPDOdb;
 	$ATMdb->db->debug=true;
@@ -21,6 +22,9 @@
 	$p->init_db_by_vars($ATMdb);
 	
 	$p=new TRH_Ressource;
+	$p->init_db_by_vars($ATMdb);
+	
+	$p=new TRH_Numero_special;
 	$p->init_db_by_vars($ATMdb);
 	
 	$p=new TRH_Contrat;
@@ -36,6 +40,9 @@
 	$ATMdb->Execute($sqlReq);
 	
 	$p=new TRH_Type_Evenement;
+	$p->init_db_by_vars($ATMdb);
+	
+	$p=new TRH_Evenement_appel;
 	$p->init_db_by_vars($ATMdb);
 	
 	$p=new TRH_Ressource_Regle;
