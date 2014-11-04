0cf54f33463ec56167e674a03839b2b75ee5a041
Alexis Algoud <alexis@atm-consulting.fr>
2014-11-04 11:08:46 +0100

    Ajout du mail pour minimum

---
+++ absence/tpl/mail.absence.alert.minimum.tpl.php
@@ -0,0 +1,6 @@
+<p>Bonjour,</p>
+
+<p>Ce mail vous informe que l'absence de ce collaborateur <strong>[mail.collabName;strconv=no;protect=no]</strong> pour [mail.DateInterval;strconv=no]
+	entraîne un défaut de personnel pour le groupe [mail.groupName] (minimum requis de [mail.minimum])</p>
+
+<p>Bonne journée.</p>
