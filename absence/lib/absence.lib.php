2b7919dced839f04c2bb03b2fc96ccb8cdf23d94
Alexis Algoud <alexis@atm-consulting.fr>
2014-12-16 15:59:49 +0100

    correction libelle

--- absence/lib/absence.lib.php
+++ absence/lib/absence.lib.php
@@ -771,15 +771,16 @@ function _getSQLListValidation($userid) {
 	if($k==1){		//on n'a qu'un groupe de validation
 		$sql=" SELECT DISTINCT u.fk_user, 
 				a.rowid as 'ID', a.date_cre  as 'DateCre',a.date_debut, a.date_fin, a.duree,
-			  	a.libelle,a.fk_user,  s.firstname, s.lastname,
+			  	ta.libelleAbsence as libelle,a.fk_user,  s.firstname, s.lastname,
 			 	a.libelleEtat as 'Statut demande', a.avertissement
 				FROM `".MAIN_DB_PREFIX."rh_valideur_groupe` as v, ".MAIN_DB_PREFIX."usergroup_user as u, 
-				".MAIN_DB_PREFIX."rh_absence as a, ".MAIN_DB_PREFIX."user as s
+				".MAIN_DB_PREFIX."rh_absence as a, ".MAIN_DB_PREFIX."user as s,".MAIN_DB_PREFIX."rh_type_absence as ta 
 				WHERE v.fk_user=".$userid." 
 				AND v.fk_usergroup=u.fk_usergroup
 				AND u.fk_user=a.fk_user 
 				AND u.fk_user=s.rowid
 				AND a.etat LIKE 'AValider'
+				AND ta.typeAbsence=a.type
 				AND v.fk_usergroup=".$TabGroupe[0]['fk_usergroup'];
 				
 				if($TabGroupe[0]['level']==1){	//on teste le niveau de validation : si il est de niveau 1, il faut qu'il puisse voir le 2 et 3
