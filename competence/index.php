01f24978dee2a6215015e3fe0bf3dd6559b054d8
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2015-11-18 14:28:47 +0100

    FIX version modif salarié

--- competence/index.php
+++ competence/index.php
@@ -1,7 +1,7 @@
 <?php
 	require('default.config.php');
 	$dol_version = (float) DOL_VERSION;
-	if($dol_version > 3.7)
+	if($dol_version >= 3.7)
 		header('location:'.DOL_URL_ROOT.'/user/card.php?id='.$user->id);
 	else
 		header('location:'.DOL_URL_ROOT.'/user/fiche.php?id='.$user->id);
