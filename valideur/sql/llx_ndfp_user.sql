c499e8305765d320e427069e0069e80d8b45b091
Alexis Algoud <alexis@atm-consulting.fr>
2016-03-01 17:21:32 +0100

    Extract Valideur

--- valideur/sql/llx_ndfp_user.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `llx_user` ADD fk_user_delegation INT DEFAULT 0;
-ALTER TABLE `llx_user` ADD code_analytique INT DEFAULT 0;
--- valideur/tpl/mail.validation.attente.tpl.php
+++
@@ -1,6 +0,0 @@
-Note de Frais
-<hr>
-
-<p>Bonjour [validation.prenom;strconv=no;protect=no] [validation.nom;strconv=no;protect=no].</p>
-
-<p>Vous avez <strong>[validation.nbr;strconv=no;protect=no]</strong> note(s) de frais en attente de validation.</p>
